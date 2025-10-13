26f2b37cce20817243957a04549d27cf0ded2854
MaxFarymets <max.pharymets@gmail.com>
2025-10-13 12:44:11 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showTime() {
-    let semestr = new Date(2025, 8, 1);
-    let birthday = new Date(2025,11,22);
-    let days = birthday.getDate() - semestr.getDate();
-    let months = birthday.getMonth() - semestr.getMonth();
-    let year = birthday.getFullYear() - semestr.getFullYear();
-    document.getElementById("time").innerHTML = "Days: " + days + " months: " + months + " years: " + year;
-}
